8c9e3a2aceb931516f818368e8a7a8decb7a435d
Juansecu <juansecoder@hotmail.com>
2021-04-04 20:01:27 -0500

    Showing some data with Thymeleaf.

--- src/main/java/com/juansecu/HeySpring/IndexController.java
+++ src/main/java/com/juansecu/HeySpring/IndexController.java
@@ -1,16 +1,43 @@
 package com.juansecu.HeySpring;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 @Slf4j
 public class IndexController {
+    @Value("${index.greeting}")
+    private String greeting;
+
     @GetMapping("/")
-    public String start() {
-        log.info("Ejecutando el controlador rest.");
-        log.debug("Más detalles del controlador");
-        return "Hey World with Spring!!!";
+    public String start(Model model) {
+        var message = "Hola Mundo con Thymeleaf.";
+        var person1 = new Person();
+        var person2 = new Person();
+        List<Person> people = Arrays.asList(person1, person2);
+        // ArrayList<Person> people = new ArrayList();
+
+        person1.setFirstName("Juanse");
+        person1.setLastName("Coder");
+        person1.setEmail("not-reply@juansecu.com");
+        person1.setPhone("5544332211");
+
+        person2.setFirstName("Karla");
+        person2.setLastName("Gomez");
+        person2.setEmail("kgomez@yahoo.com");
+        person2.setPhone("9988776655");
+
+        log.info("Ejecutando el controlador de tipo Spring MVC.");
+        model.addAttribute("message", message);
+        model.addAttribute("greeting", this.greeting);
+        // model.addAttribute("person1", person1);
+        model.addAttribute("people", people);
+
+        return "index";
     }
 }
---
+++ src/main/java/com/juansecu/HeySpring/Person.java
@@ -0,0 +1,11 @@
+package com.juansecu.HeySpring;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+}
